a6749c98b865d767171e328f0bda83786c6aac03
Alexander Fedotov <53221815+sashafdtv@users.noreply.github.com>
2021-08-08 15:11:24 +0300

    version 1.0

---
+++ js/index.js
@@ -0,0 +1,83 @@
+{
+    /* Быстро получение DOM узлов */
+    const getNode = selector => document.querySelector(selector);
+    const getNodes = selector => document.querySelectorAll(selector);
+
+    /* Получаем название таски из DOM */
+    const getTitle = () => getNode(".input.title textarea").value;
+
+    /* Получаем описание таски из DOM */
+    const getNotes = () => {
+        const paragraphs = getNodes(".info .text-editor .ProseMirror p");
+
+        const result = [...paragraphs].map(paragraph => {
+            const separators = [...paragraph.children].filter(children => children.tagName === "BR");
+            [...separators].forEach(separator => separator.replaceWith(" \r\n"));
+            return paragraph;
+        });
+        return [...result].map(p => p.textContent.trim() || "\r\n \r\n").join("").trim();
+    }
+
+    /* Получаем сабстаски из DOM */
+    const getItems = () => {
+        const tasks = getNodes(".task-checklist .checklist-item .task-link");
+        return [...tasks].map(task => task.textContent.trim());
+    }
+
+    /* Трансформация массива с названиями подзадач в JSON для things 3 */
+    const transformItems = arr => {
+        return arr.map(task => {
+            return {
+                "type": "to-do",
+                "attributes": {
+                    "title": task
+                }
+            }
+        })
+    }
+
+
+    /* Отправлям таску в things 3 */
+    window.open("things:///json?data=" +
+        encodeURI(JSON.stringify([
+            {
+                "type": "project",
+                "attributes": {
+                    "title": `${getTitle()}`,
+                    "notes": `${document.location.href + ' \r\n \r\n'}${getNotes()}`,
+                    "area": "Roistat",
+                    "items": [
+                        {
+                            "type": "heading",
+                            "attributes": {
+                                "title": "Сабтаски"
+                            }
+                        }
+                    ].concat(transformItems(getItems()))
+                        .concat([
+                            {
+                                "type": "heading",
+                                "attributes": {
+                                    "title": "Отгрузка"
+                                }
+                            },
+                            {
+                                "type": "to-do",
+                                "attributes": {
+                                    "title": "Ревью",
+                                    "tags": ["Блок"]
+                                }
+                            },
+                            {
+                                "type": "to-do",
+                                "attributes": {
+                                    "title": "Отгрузка",
+                                    "tags": ["Блок"]
+                                }
+                            }
+                        ])
+                }
+            },
+        ]))
+    )
+}
