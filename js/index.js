fc266ef9ee9815e2c64b503d50556ad274f7116f
afdtv <fedotov.a@roistat.com>
2022-10-09 15:24:43 +0300

    обновил картинки, добавил сабтаски на логи и отгрузку

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 {
+    const TODO_KEY = 'to-do',
+          HEADER_KEY = 'header';
+
     /* Быстрое получение DOM узлов */
     const _getNode = selector => document.querySelector(selector);
     const _getNodes = selector => document.querySelectorAll(selector);
 
     /* Получаем название таски из DOM */
-    const getTitle = () => _getNode(".input.title textarea").value;
+    const getTaskTitle = () => _getNode(".input.title textarea").value;
 
     /* Получаем описание таски из DOM */
-    const getNotes = () => {
+    const getTaskDescription = () => {
         const paragraphs = _getNodes(".info .fields ~ .text-editor .ProseMirror p");
 
         const result = [...paragraphs].map(paragraph => {
@@ -20,16 +23,16 @@
     }
 
     /* Получаем сабстаски из DOM */
-    const getItems = () => {
+    const getSubtasks = () => {
         const tasks = _getNodes(".task-checklist .checklist-item .task-link");
         return [...tasks].map(task => task.textContent.trim());
     }
 
     /* Трансформация массива с названиями подзадач в JSON для things 3 */
-    const transformItems = arr => {
+    const getFormattedTasks = (arr, type) => {
         return arr.map(task => {
             return {
-                "type": "to-do",
+                "type": type,
                 "attributes": {
                     "title": task
                 }
@@ -37,24 +40,31 @@
         })
     }
 
+    const TASK_HREF = `${document.location.href + ' \r\n \r\n'}${getTaskDescription()}`
+    const TASK_TITLE = `${getTaskTitle()}`
+    const TASK_SUBTASKS = getFormattedTasks(getSubtasks())
+
+    const TASK_RELEASE_SUBTASK = getFormattedTasks(['Отгрузка'], TODO_KEY);
+    const TASK_LOG_SABTASK = [
+        ...getFormattedTasks(['Логирование'], HEADER_KEY),
+        ...getFormattedTasks(['Общие логи по задаче'], TODO_KEY),
+    ]
+
+
+
     /* Формируем JSON */
     const projectJSON = "things:///json?data=" +
         encodeURI(JSON.stringify([
             {
                 "type": "project",
                 "attributes": {
-                    "title": `${getTitle()}`,
-                    "notes": `${document.location.href + ' \r\n \r\n'}${getNotes()}`,
+                    "title": TASK_TITLE,
+                    "notes": TASK_HREF,
                     "items": [
-                        {
-                            "type": "heading",
-                            "attributes": {
-                                "title": "Сабтаски"
-                            }
-                        }
+                        ...TASK_SUBTASKS,
+                        ...TASK_RELEASE_SUBTASK,
+                        ...TASK_LOG_SABTASK,
                     ]
-                        /* Сабтаски */
-                        .concat(transformItems(getItems()))
                 }
             },
         ]));
